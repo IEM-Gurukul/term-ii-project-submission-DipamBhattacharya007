69b234dbd9ce41f99044460be8082af9cbc705f4
DipamBhattacharya007 <dipam2134@gmail.com>
2026-03-28 00:45:43 +0530

    I am adding Book class with attributes and constructor

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-class Book {}
+class Book {
+   private int bookId;
+    private String title;
+    private String author;
+    private boolean isIssued;
+
+    public Book(int bookId, String title, String author){
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.isIssued = false;
+    }
+}
 class Library {}
-public class LibraryManagementSystem {}
+public class LibraryManagementSystem {
+  public static void main(String[] args){
+  }
+}
