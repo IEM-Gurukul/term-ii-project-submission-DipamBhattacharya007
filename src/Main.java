d61ca53fffb9272f47bdcb8eee585037a1d64503
DipamBhattacharya007 <dipam2134@gmail.com>
2026-03-28 08:34:49 +0530

    I have implemented getter methods in Book class

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,31 @@ import java.util.Scanner;
 
 class Book {
    private int bookId;
-    private String title;
-    private String author;
-    private boolean isIssued;
+   private String title;
+   private String author;
+   private boolean isIssued;
 
-    public Book(int bookId, String title, String author){
-        this.bookId = bookId;
-        this.title = title;
-        this.author = author;
-        this.isIssued = false;
-    }
+   public Book(int bookId, String title, String author){
+      this.bookId = bookId;
+      this.title = title;
+      this.author = author;
+      this.isIssued = false;
+   }
+   public int getBookId() {
+       return bookId;
+   }
+
+   public String getTitle() {
+       return title;
+   }
+
+   public String getAuthor() {
+       return author;
+   }
+
+   public boolean isIssued() {
+       return isIssued;
+   }
 }
 class Library {}
 public class LibraryManagementSystem {
