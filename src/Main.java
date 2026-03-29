36b050b6fb473b388011673f0893e0ad287eb76d
DipamBhattacharya007 <dipam2134@gmail.com>
2026-03-29 20:04:52 +0530

    I have added main method with menu-driven user interface

--- src/Main.java
+++ src/Main.java
@@ -104,5 +104,73 @@ class Library {
    }
 public class LibraryManagementSystem {
   public static void main(String[] args){
+     Scanner sc = new Scanner(System.in);
+        Library library = new Library();
+
+        
+        library.addBook(new Book(1, "Java Programming", "James Gosling"));
+        library.addBook(new Book(2, "Data Structures", "Schaum"));
+        library.addBook(new Book(3, "Operating System", "Galvin"));
+
+        int choice;
+
+        do {
+            System.out.println("\n===== Library Management System =====");
+            System.out.println("1. Add Book");
+            System.out.println("2. View All Books");
+            System.out.println("3. Search Book");
+            System.out.println("4. Issue Book");
+            System.out.println("5. Return Book");
+            System.out.println("6. Exit");
+            System.out.print("Enter your choice: ");
+            choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter Book ID: ");
+                    int id = sc.nextInt();
+                    sc.nextLine();
+
+                    System.out.print("Enter Book Title: ");
+                    String title = sc.nextLine();
+
+                    System.out.print("Enter Author Name: ");
+                    String author = sc.nextLine();
+
+                    library.addBook(new Book(id, title, author));
+                    break;
+
+                case 2:
+                    library.viewBooks();
+                    break;
+
+                case 3:
+                    System.out.print("Enter Book ID to search: ");
+                    int searchId = sc.nextInt();
+                    library.searchBook(searchId);
+                    break;
+
+                case 4:
+                    System.out.print("Enter Book ID to issue: ");
+                    int issueId = sc.nextInt();
+                    library.issueBook(issueId);
+                    break;
+
+                case 5:
+                    System.out.print("Enter Book ID to return: ");
+                    int returnId = sc.nextInt();
+                    library.returnBook(returnId);
+                    break;
+
+                case 6:
+                    System.out.println("Exiting program...");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice. Try again.");
+            }
+        } while (choice != 6);
+
+        sc.close();
   }
 }
